149404b1eee6f9487c7e6bb8333fc86946ee9ffa
David-Ykz <kzdavid314@gmail.com>
2024-09-11 23:59:18 -0400

    fixed frontend to display feed correctly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useRef } from 'react';
+
+
 
 function App() {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    videoRef.current.src = 'http://192.168.50.179:5000/video_feed';
+  }, []);
+  
+  function onButtonClick() {
+    fetch('http://192.168.50.179:5000/test')
+    .then((res) => res.text())         // Change to .json() if the response is JSON
+    .then((data) => {
+      console.log(data);               // Store the response in the state
+    })
+    .catch((err) => {
+      console.error("Error fetching data: ", err);
+    });
+  }
+
+
   return (
     <div className="App">
       <h1>Camera Feed</h1>
-      <video
-        src="http://192.168.50.179:5000/video_feed"
-        width="600"
-        height="400"
-        autoPlay
-      />
+      <img ref={videoRef} alt="Video feed" />
+      
+      <button onClick={onButtonClick}>
+        test
+      </button>
     </div>)
 }
 
