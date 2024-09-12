1fc829db0a58dae488fa935db65c61a3d0ab7df1
David-Ykz <kzdavid314@gmail.com>
2024-09-12 03:58:49 +0000

    fixed video stream to frontend

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,9 +1,12 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
+// g++ -std=c++11 -shared -o video_capture.so main.cpp -fPIC $(pkg-config --cflags --libs opencv4)
+
 extern "C" {
     int capture_frame(unsigned char** data, int* size) {
-        cv::VideoCapture cap(1);
+        //cv::VideoCapture cap(1);
+        cv::VideoCapture cap(1, cv::CAP_V4L2);
 
         if (!cap.isOpened()) {
             std::cerr << "Error: Could not open the camera." << std::endl;
