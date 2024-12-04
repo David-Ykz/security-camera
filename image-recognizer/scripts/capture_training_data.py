9832ee51c03996adb04a99ae69a3d0a1ad162cdf
David-Ykz <kzdavid314@gmail.com>
2024-12-04 02:59:44 +0000

    add scripts

---
+++ image-recognizer/scripts/capture_training_data.py
@@ -0,0 +1,14 @@
+import cv2
+import time
+
+cap = cv2.VideoCapture(1)
+NUM_PICTURES = 20
+LABEL = "Locked"
+
+for i in range(NUM_PICTURES):
+    time.sleep(1)
+    ret, frame = cap.read()
+    
+    cv2.imwrite(f"{LABEL}/{i+1}.jpg", frame)    
+
+cap.release()
